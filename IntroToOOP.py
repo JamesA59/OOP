8e0999b3f7ff4cd6e3e6698d95a166e84d43e444
JamesA59 <jamesba555@yahoo.com>
2024-05-08 15:46:03 -0500

    notes

--- IntroToOOP.py
+++ IntroToOOP.py
@@ -1,1 +1,22 @@
 # Introduction to Object Oriented Programming in Python
+
+# git config user.name "JamesA59"
+# git config user.email "jamesba555@yahoo.com"
+# Programming paradigm - an approach to organize and structure your code
+# 2 common programming paradigms: Procedural programming and object oriented programming (OOP)
+# Procedural programming - a step by step procedure is followed sequentially to solve a problem
+# ex. C, 
+# OOP - Real world entities or concepts are modeled using objects
+# ex. Java, C#
+# Python is flexible and able to support both paradigms
+# Classes are blueprints for creating objects, specifys what data the object will have and what behaviors will be exhibited
+# When we define a class, we create a new type, the values are instances and objects
+# Instantation - creating a instance of a class
+# Encapsulation- classes encapsulate state and behavior together
+# State is maintained through variables
+# Behaviors are implemented through methods
+# A benefit of OOP is code reuseability
+# You can inherit from existing classes to make new classes
+# Data encapsulation gives extra data security
+# Abstraction- Hide inner workings of system from user
+# Polymorphism - one thing in many forms- can be implemented
