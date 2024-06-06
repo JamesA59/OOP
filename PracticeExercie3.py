69925ad1c6d588141f68eb9609430f72aed5c8df
JamesA59 <jamesba555@yahoo.com>
2024-06-06 13:24:38 -0500

    done?

--- PracticeExercie3.py
+++ PracticeExercie3.py
@@ -59,13 +59,18 @@ f3.show()
 
 class SalesPerson:   
 
+    names = []
+    total_revenue = 0
+
     def __init__(self,name,age):
         self.name = name
         self.age = age
         self.sales_amount = 0
+        SalesPerson.names.append(name)
  
     def make_sale(self,money):
         self.sales_amount += money
+        SalesPerson.total_revenue += money
  
     def show(self):
         print(self.name, self.age, self.sales_amount)
@@ -79,17 +84,137 @@ s1.make_sale(1200)
 s2.make_sale(5000)
 s3.make_sale(3000)
 s3.make_sale(8000)
-
-SalesPeople = [s1,s2,s3]
-total_revenue = 0
-names = []
-names = [i.name for i in SalesPeople]
-for i in SalesPeople:
-    total_revenue += i.sales_amount
  
 s1.show()
 s2.show()
 s3.show()
 
-print(names)
-print(total_revenue)
+print(SalesPerson.names)
+print(SalesPerson.total_revenue)
+
+class Employee: 
+
+    Domains = set()
+    allowed_domains = {'yahoo.com', 'gmail.com', 'outlook.com'}
+
+    def __init__(self,name,email):
+        self.name = name
+        self.email = email
+        domain = email[email.index('@')+1 : ]
+        Employee.Domains.add(domain)
+
+    @property
+    def email(self):
+        return self._email
+    
+    @email.setter
+    def email(self, new_email):
+        domain = new_email[new_email.index('@')+1 : ]
+        if domain in Employee.allowed_domains:
+            self._email = new_email
+        else:
+            raise RuntimeError(f'Domain {domain} is not allowed')
+
+    def display(self):
+        print(self.name, self.email)
+            
+e1 = Employee('John','john@gmail.com')
+e2 = Employee('Jack','jack@yahoo.com')
+e3 = Employee('Jill','jill@outlook.com')
+e4 = Employee('Ted','ted@yahoo.com')
+e5 = Employee('Tim','tim@gmail.com')
+e6 = Employee('Mike','mike@yahoo.com')
+
+print(Employee.Domains)
+'''
+e4.email = 'ted@ymail.com'
+e4.display()
+'''
+ 
+e3.email = 'jill@gmail.com'
+e3.display()
+
+
+'''
+class Stack:    
+
+    MAX_SIZE = 7
+
+    def __init__(self):
+        self.items = []
+ 
+    def is_empty(self):
+        return self.items == []
+ 
+    def size(self):
+        return len(self.items)
+ 
+    def push(self, item):
+        if self.size == Stack.MAX_SIZE:
+            raise RuntimeError("Stack is full")
+        self.items.append(item)
+ 
+    def pop(self):
+        if self.is_empty():
+            raise RuntimeError("Stack is empty")
+        return self.items.pop()
+    
+    def display(self):
+        print(self.items)
+ 
+if __name__ == "__main__":
+    st = Stack()
+ 
+    while True:
+        print("1.Push") 
+        print("2.Pop") 
+        print("3.Peek") 
+        print("4.Size")
+        print("5.Display") 
+        print("6.Quit")
+         
+        choice = int(input("Enter your choice : "))
+ 
+        if choice == 1:
+            x=int(input("Enter the element to be pushed : "))
+            st.push(x) 
+        elif choice == 2:
+            x=st.pop() 
+            print("Popped element is : " , x) 
+        elif choice == 3:
+            print("Element at the top is : " , st.peek()) 
+        elif choice == 4:
+            print("Size of stack " , st.size()) 
+        elif choice == 5:
+            st.display()         
+        elif choice == 6:
+          break;
+        else:
+          print("Wrong choice") 
+        print() 
+'''
+
+
+class BankAccount:
+ 
+    bank_name = "Chase"
+
+    def __init__(self, name, balance=0, bank=bank_name):
+        self.name = name
+        self.balance = balance
+        self.bank = bank
+        
+    def display(self):
+         print(self.name, self.balance, self.bank)
+ 
+    def withdraw(self, amount):
+        self.balance -= amount
+ 
+    def deposit(self, amount):
+        self.balance += amount
+    
+a1 = BankAccount('Mike', 200)
+a2 = BankAccount('Tom')
+ 
+a1.display()
+a2.display()
