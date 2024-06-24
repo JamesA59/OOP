e23bbb8ecf65274018f81f74ad572afe47a275a3
JamesA59 <jamesba555@yahoo.com>
2024-06-24 10:59:33 -0500

    wip2

--- PracticeExercise5.py
+++ PracticeExercise5.py
@@ -2,24 +2,36 @@
 
 class Course:
 
-    def __init__(self, title, instuctor, price, lectures, users, rating, avg_rating):
+    def __init__(self, title, instuctor, price, lectures):
         self.title = title
         self.instrucor = instuctor
         self.price = price
         self.lecturers = lectures
         self.users = []
-        self.rating = rating
-        self.avg_rating = avg_rating
+        self.ratings = []
+        self.avg_rating = 0
 
     def __str__(self):
-        pass
+        return f'{self.title}, {self.instrucor}, {self.price}, {self.lecturers}'
 
     def new_user_enrolled(self,u):
         self.users.append(u)
-        #return self.users
+        return self.users
 
-    def received_a_rating(self):
-        pass
+    def received_a_rating(self, r):
+        self.ratings.append(r)
+        self.avg_rating = sum(self.ratings) / len(self.ratings)
+        return self.ratings, self.avg_rating
+        
 
     def show_details(self):
-        pass
+        print("Title :", self.title, "Instructor: ", self.instrucor, "Price: ", self.price, "Lecturers: ", self.lecturers, 
+              "Users: ", self.users, "Ratings: ", self.ratings, "Average Ratings: ", self.avg_rating)
+
+c1 = Course("Project Management", "Ramsower", "$50", "2")
+c1.new_user_enrolled("James")
+c1.received_a_rating(8)
+c1.new_user_enrolled("Natasha")
+c1.received_a_rating(7)
+c1.show_details()
+ 
